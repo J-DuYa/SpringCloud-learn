25b62a7cf7a1456820110729f1a03a7cff21944d
毒牙 <1052363946@qq.com>
2019-10-14 20:24:17 +0800

    add

--- eureka-server/src/main/java/com/duya/java/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/duya/java/eurekaserver/EurekaServerApplication.java
@@ -2,11 +2,14 @@ package com.duya.java.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
